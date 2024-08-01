0f1ff02fba4f4cbebad49c4e38b99c6b8a488f3d
GabrielF-S <gabrielboni2008@hotmail.com>
2024-08-01 11:48:55 -0300

    Criado EurekaServer

--- user-api/src/main/java/br/com/gfsantos/user_api/services/impl/UserServiceImpl.java
+++ user-api/src/main/java/br/com/gfsantos/user_api/services/impl/UserServiceImpl.java
@@ -5,20 +5,23 @@ import br.com.gfsantos.user_api.repositories.UserRepository;
 import br.com.gfsantos.user_api.services.UserService;
 import br.com.gfsantos.user_api.services.exceptions.ObjectNotFoundException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class UserServiceImpl implements UserService {
 
     @Autowired
     private final UserRepository userRepository;
-
+    private final Environment env;
     @Override
     public User findById(Long id) {
+        log.info("USER_SERVICE ::: GET request on " + env.getProperty("local.server.port") + " port");
         return   userRepository.findById(id).orElseThrow(() -> new ObjectNotFoundException("Object not found"));
     }
 
